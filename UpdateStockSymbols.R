0010b07cebe3eb2642601e391bd88ec0a7b9bf5d
domblou <dominic_blouin@hotmail.com>
2018-09-12 19:28:23 -0400

    Add

---
+++ UpdateStockSymbols.R
@@ -0,0 +1,18 @@
+####
+#### The objective of this script is to create a list of StockSymbols and save it in .csv
+####
+
+#### Get symbols from http://eoddata.com/stocklist/TSX/A.htm
+library(XML)
+
+#### Parameters
+StockSymbolsFile <- "StockSymbolsTMX.csv"
+
+Symbols <- NULL
+for (i in LETTERS){
+  
+  url = paste("http://eoddata.com/stocklist/TSX/", i, ".htm", sep = "")
+  Symbols = append(Symbols, readHTMLTable(url, header=F, which=6, stringsAsFactors=F)[,1])
+}
+
+write.csv(Symbols[2:length(Symbols)], StockSymbolsFile, row.names = FALSE)
